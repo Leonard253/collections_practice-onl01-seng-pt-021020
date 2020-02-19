b262d4d9fe74675114e2834e0347c383842c2a8d
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-19 06:42:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,13 +21,7 @@ def reverse_array(array)
   array.reverse
 end
 
-def kesha_maker(array)
-  array.each do |item|
-    item[2] = "$"
-    return_array << item
-  end
-  return_array
-end
+
 
 def find_a(array)
    array.find_all do |word|
